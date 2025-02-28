1c7a66fe748fea34dde40e1638dd95f33b5722f5
Matthew Gilbert <105068266+mlgilb@users.noreply.github.com>
2025-02-27 19:30:05 -0500

    Update process_A.cpp

--- process_A.cpp
+++ process_A.cpp
@@ -3,7 +3,6 @@
 #include <sys/shm.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
 #define INT_KEY 1234
 #define STR_KEY 5678
@@ -23,17 +22,28 @@ int main() {
     *shared_pid = getpid();
     printf("%d %s whose id is: %d\n", *shared_int, shared_str, *shared_pid);
 
-    while (*shared_int < 30) {
+    int printed_B = 0; // Flag to track Process B output
+    int printed_C = 0; // Flag to track Process C output
+
+    while (!printed_C) {  // Keep looping until Process C updates shared memory
+        if (*shared_int == 20 && !printed_B) {  // Print B only once
+            printf("%d %s whose id is: %d\n", *shared_int, shared_str, *shared_pid);
+            printed_B = 1;
+        }
+
+        if (*shared_int == 30 && !printed_C) {  // Print C only once
+            printf("%d %s whose id is: %d\n", *shared_int, shared_str, *shared_pid);
+            printed_C = 1;
+        }
+
         sleep(1);
     }
 
-    printf("%d %s whose id is: %d\n", *shared_int, shared_str, *shared_pid);
     printf("Good Bye World, I am Done!!\n");
 
     shmdt(shared_int);
     shmdt(shared_str);
     shmdt(shared_pid);
-
     shmctl(shmid_int, IPC_RMID, NULL);
     shmctl(shmid_str, IPC_RMID, NULL);
     shmctl(shmid_pid, IPC_RMID, NULL);
